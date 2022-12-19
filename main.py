ad9d61b54c10bfa3bb8c9dc9ceed3e0b11861346
ragnarlodbrok1992 <maraus92@gmail.com>
2022-12-19 19:53:36 +0100

    Prototyping drawing and dragging grid

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import pygame
 RES_HEIGHT = 800
 RES_WIDTH = 600
 
+# Grid values
+TILE_SIZE = 20
+GRID_ROWS = 21
+GRID_COLUMNS = 30 
+
 # Some colors
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
@@ -11,9 +16,37 @@ GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+# <------y------->
+# ^
+# -
+# x
+# -
+# v
+
+# Tile class
+class Tile:
+    def __init__(self, pos_x, pos_y, size=TILE_SIZE):
+        self.nw = (pos_x, pos_y)
+        self.ne = (pos_x, pos_y + size)
+        self.sw = (pos_x + size, pos_y)
+        self.se = (pos_x + size, pos_y + size)
+        self.rect = pygame.Rect(self.nw, (size, size))
+
+        # DEBUG
+        # print(self.nw)
+        # print(self.ne)
+        # print(self.sw)
+        # print(self.se)
+        # print(self.rect)
+
+
 # Creating test grid
 def create_grid(height: int, width: int) -> list:
-    pass
+    grid_output = []
+    for x in range(0, height):
+        for y in range(0, width):
+            grid_output.append(Tile(x * TILE_SIZE, y * TILE_SIZE))
+    return grid_output
 
 # Prototyping functions - rotating grid
 def rotate_grid(angle_in_rads: float) -> None:
@@ -32,14 +65,28 @@ class Engine():
         self.game_loop = True
 
         # Prototyping variables
-        self.camera = (0, 0)
-        self.grid = []
+        self.camera = [0, 0]
+        self.grid = create_grid(GRID_COLUMNS, GRID_ROWS)
+        self.left_mouse_button_held = False
+
+        # Debug
+        # Tile(0, 0)
+        # print(self.grid)
 
     def run(self):
         self.main_game_loop()
 
     def render_grid(self):
-        pass
+        # Render grid
+        for index, tile in enumerate(self.grid):
+            # Change rects positions
+            tile.rect.move_ip(self.camera[0], self.camera[1])
+            if index % 2 == 0:
+                pygame.draw.rect(self.screen, GREEN, tile.rect)
+            else:
+                pygame.draw.rect(self.screen, RED, tile.rect)
+        # Zero the camera
+        self.camera = [0, 0]
 
     def main_game_loop(self):
         while self.game_loop:
@@ -49,8 +96,44 @@ class Engine():
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
                         self.game_loop = False
+                        """
+                    elif event.key == pygame.K_a:
+                        # print("Pressed a!")
+                        self.camera[0] -= 10
+                    elif event.key == pygame.K_d:
+                        # print("Pressed d!")
+                        self.camera[0] += 10
+                    elif event.key == pygame.K_w:
+                        # print("Pressed w!")
+                        self.camera[1] -= 10
+                    elif event.key == pygame.K_s:
+                        # print("Pressed s!")
+                        self.camera[1] += 10
+                        """
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pressed_keys = pygame.mouse.get_pressed()
+                    self.left_mouse_button_held = mouse_pressed_keys[0]
+                    # mouse_pos_rel = pygame.mouse.get_rel()
+                    print(mouse_pressed_keys)
+                    # print(mouse_pos_rel)
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    self.left_mouse_button_held = False
+            # Check if mouse button is held
+            if self.left_mouse_button_held:
+                # print("Holding left mouse button!")
+                # print(pygame.mouse.get_rel())
+                mouse_rel = pygame.mouse.get_rel()
+                self.camera[0] += mouse_rel[0]
+                self.camera[1] += mouse_rel[1]
+            else:
+                pygame.mouse.get_rel()
+
             # Render frame here
+
             self.screen.fill(BLACK)
+
+            # Render grid
+            self.render_grid()
 
             # Do at the end of frame
             pygame.display.update()
